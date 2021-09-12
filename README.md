1bde7554afc32f1ae27024cd3812e93fc05b9c81
icyray <hoyo.chen.i@gmail.com>
2021-09-12 20:19:09 +0800

    Add firmware check

---
+++ README.md
@@ -0,0 +1,7 @@
+# Enco X Downgrade Server
+
+OPPO Enco X / W51 降级代理服务器。
+
+放置 `firmware.bin` 至程序同目录，运行程序后在手机上配置代理，使用降级版欢律刷入固件更新。
+
+仅在 Enco X 上测试通过，不保证其他设备的可用性。
--- downgradeServer.py
+++ downgradeServer.py
@@ -5,6 +5,8 @@ from http.server import SimpleHTTPRequestHandler
 import socketserver
 import json
 import os
+import sys
+import re
 
 PORT = 8090
 data = {
@@ -12,16 +14,17 @@ data = {
     'data': {
         'content': [{
             'name': '1_all_500_0.bin',
-            #'size': firmware_size,
+            'size': '__firmware_size',
             'url': 'http://sdk-smarthome.nearme.com.cn/firmware.bin'
         }],
-        'name': 'OPPO Enco X',
-        'productId': '061410',
+        'name': '__product_name',
+        'productId': '__product_id',
         'updateInfo': '降级代理成功！\\n酷安@icyray',
         'version': '500'
     },
     'msg': '成功'
 }
+
 
 class Proxy(SimpleHTTPRequestHandler):
     def do_POST(self):
@@ -31,7 +34,7 @@ class Proxy(SimpleHTTPRequestHandler):
             self.send_response(200)
             self.send_header('Content-type', 'application/json')
             self.end_headers()
-            if req_datas['productId'] == '061410':
+            if req_datas['productId'] == '061410' or req_datas['productId'] == '060C10':
                 self.log_message('已捕获升级请求！')
                 self.wfile.write(json.dumps(data).encode())
             else:
@@ -51,13 +54,41 @@ class Proxy(SimpleHTTPRequestHandler):
         else:
             self.wfile.write(b"HTTP/1.1 404 Not Found\r\n\r\n" + b"Not Found")
 
-def get_firmsize():
+
+def get_firminfo():
+    with open('firmware.bin', 'rb') as f:
+        f.seek(-512, os.SEEK_END)
+        fb = f.read()
+    return re.search(rb'SW_VER=(\d+)\s+[\s\S]*PID=0X(\w+)\s+', fb)
+
+
+def get_devicename(deviceID):
+    supported_devices = {'061410': 'OPPO Enco X', '060C10': 'OPPO Enco W51'}
+    return supported_devices.get(str(deviceID), '未知设备')
+
+
+def update_data(firminfo):
     data['data']['content'][0]['size'] = str(os.path.getsize('./firmware.bin'))
+    data['data']['productId'] = str(firminfo.group(2).decode())
+    data['data']['name'] = str(get_devicename(firminfo.group(2).decode()))
+
 
 if __name__ == '__main__':
-    get_firmsize()
-    socketserver.ThreadingTCPServer.allow_reuse_address = True
-    with socketserver.ThreadingTCPServer(('', PORT), Proxy) as httpd:
-        print('警告：降级操作具有危险性，失败导致设备损坏的风险由您自行承担！目前仅支持 OPPO Enco X 降级。')
-        print('已启动降级代理服务器，端口：{}'.format(PORT))
-        httpd.serve_forever()
+    try:
+        firminfo = get_firminfo()
+        if not firminfo:
+            print('无效固件或该固件已加密！')
+            sys.exit(1)
+        update_data(firminfo)
+
+        socketserver.ThreadingTCPServer.allow_reuse_address = True
+        with socketserver.ThreadingTCPServer(('', PORT), Proxy) as httpd:
+            print('警告：降级操作具有危险性，失败导致设备损坏的风险由您自行承担！目前仅支持 OPPO Enco X / W51 降级。')
+            print('请在手机上设置代理后，使用降级版欢律刷入固件。')
+            print(f'固件读取完毕，固件版本号: {firminfo.group(1).decode()}，适用设备 ID: {firminfo.group(2).decode()}，设备名: {get_devicename(firminfo.group(2).decode())}.')
+            print(f'已启动降级代理服务器，端口：{PORT}')
+            httpd.serve_forever()
+            
+    except FileNotFoundError:
+        print('请将 firmware.bin 放置在本目录下。')
+        sys.exit(1)
